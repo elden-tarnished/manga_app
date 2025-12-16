434b1c3f52eeff3a6c1fbafe96cb172a7ae24aa2
Seo <aroofzads@gmail.com>
2025-12-17 00:02:59 +0330

    fixing skeleton sizing issue

--- client/src/Skeletons/MangaCard/MangaCard.jsx
+++ client/src/Skeletons/MangaCard/MangaCard.jsx
@@ -4,24 +4,16 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
 export function MangaCardSkel(props) {
-  const {color} = props
+  const { color } = props
   const cardRef = useRef(null);
   const titleRef = useRef(null)
 
-  useGSAP(() => {
-    gsap.timeline()
-    .to(titleRef.current, {
-      backgroundPosition: '200% 100%',
-      duration: 1.6,
-      repeat:-1,
-      ease: 'power1.inOut',
-    }, 0)
-  })
-  return(
+  return (
     <div ref={cardRef} className='card--skel' style={{
       backgroundImage: `linear-gradient(135deg, ${color[0]} 30%, ${color[1]} 50%, ${color[0]} 70%)`,
       backgroundPosition: '100% 0',
-      backgroundSize: '600% 100%'}}>
+      backgroundSize: '600% 100%'
+    }}>
       <div ref={titleRef} className='title--skel'></div>
     </div>
   )
