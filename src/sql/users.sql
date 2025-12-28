5b9dce9977e62a868156995870fbb88ceed36c19
Seo <aroofzads@gmail.com>
2025-12-28 19:06:46 +0330

    db: refine manga and users table schema

--- src/sql/users.sql
+++ src/sql/users.sql
@@ -1,16 +1,12 @@
 create table users (
-	id serial primary key ,
-	username text unique not null ,
-	email text unique not null ,
+	id serial primary key,
+	username text unique not null,
+	email text unique not null,
+	password text
 );
-/*
-might add: 
-	picture text
-	name text
-	to users table later
-*/
+
 create table users_favorites (
-	users_id int references users(id) on delete cascade,
+	user_id int references users(id) on delete cascade,
 	manga_id int references manga(id) on delete cascade,
-	primary key (users_id, manga_id)
+	primary key (user_id, manga_id)
 );
